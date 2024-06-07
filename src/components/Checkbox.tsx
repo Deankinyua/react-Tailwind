bda7719ea301fc7be82559b23ba69a2585b26f46
deankinyua <kinyuadean@gmail.com>
2024-06-07 11:20:57 +0300

    used grid_templete_columns: auto 1fr to create a checkbox and its corresponding content

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,27 @@
+const Checkbox = () => {
+  return (
+    <>
+      <div className="grid auto_grid">
+        <div>
+          <label htmlFor="value">Enter</label>
+          <input type="checkbox" id="value" placeholder="check the box" />
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In sed
+          molestias at. Velit, assumenda eveniet blanditiis quasi dolorem
+          accusamus voluptatibus delectus quam alias, corrupti ex minima cum
+          similique voluptatum quibusdam expedita doloremque beatae provident
+          laudantium incidunt doloribus magni praesentium culpa? Quasi dolores,
+          quam in animi ullam vero neque sunt eum at voluptatum rerum accusamus.
+          Sit explicabo veniam rem esse porro. Veniam possimus temporibus
+          minima, dolore esse iure incidunt reprehenderit, iusto ab perspiciatis
+          nobis excepturi ratione recusandae amet est quia expedita vero ad non
+          enim eius architecto corporis? Nesciunt soluta provident, voluptatibus
+          aliquid perspiciatis quae consequatur ad error! Hic, veritatis quis?
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkbox;
