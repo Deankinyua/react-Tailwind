b50d769923457eccdfe7fe0f77647789b16848e6
deankinyua <kinyuadean@gmail.com>
2024-06-07 15:52:30 +0300

    dhdhdh

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,40 @@
-import { ReactNode } from "react";
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { ReactNode, useState } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
 const Alert = ({ children }: Props) => {
+  const [heartState, setHeartState] = useState(false);
+
   return (
     <>
       <div>This are the {children}</div>
+      <div>
+        {heartState && (
+          <AiFillHeart
+            color="#ff6b81"
+            size={20}
+            onClick={() => {
+              setHeartState(false);
+              console.log("unliked");
+            }}
+          />
+        )}
+      </div>
+      <div>
+        {!heartState && (
+          <AiOutlineHeart
+            size={20}
+            onClick={() => {
+              setHeartState(true);
+              console.log("liked");
+            }}
+          />
+        )}
+      </div>
     </>
   );
 };
